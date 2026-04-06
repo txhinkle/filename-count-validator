745be79b70605c05937f21c5e38ddcd1647d793d
Taz Hinkle <thinkle@utah.gov>
2026-04-06 10:12:58 -0600

    +stream

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Move the file you are validating into the same folder as validator.js
-Copy the file name into the single quotes on line 3
-Enter the fileType into the single quotes on line 5
+Copy the file name into the single quotes on line 4
+Enter the fileType into the single quotes on line 6
   - the regexObject will list which filetypes can be validated
--- validator.js
+++ validator.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs');
+const {open} = require('node:fs/promises');
 // copy and paste the file name, including extention into the quotes below
-const filename = 'EREP_MEMBER_ELIGIBILITY_IN_REALTIME_01132026_1768318547471_T.txt'
+const filename = ''
 // Enter the name of the interface the file is for- e.g. 911
 const fileType = '911'
 const dateString = new Date(Date.now()).toLocaleString().split(',')[0];
@@ -11,7 +12,7 @@ const date = dateArray[0] + dateArray[1] + dateArray[2];
 
 const regexObject = {
     911: new RegExp("EREP\_MEMBER\_ELIGIBILITY\_IN\_REALTIME\_" + date + "\_[0-9 A-Z a-z]*\_T\.txt"),
-    934: new RegExp("EREP\_MEMBER\_ELIGIBILITY\_IN\_BATCH\_" + date + "\_[0-9 A-Z a-z]*\_T\.txt"),
+    934: new RegExp("EREP\_MEMBER\_ELIGIBILITY\_IN\_BATCH\_" + date + '\_[0-9]' + "\_[0-9 A-Z a-z]*\_T\.txt"),
     1501: new RegExp('ORSIS\_TPL\_INFO\_IN\_' + date + '_[0-9]{4}_TEST\.xml'),
     424: new RegExp('PURCHASED\_DHS\_SERVICES\_CLAIMS\_FROM\_CAPS\_IN\_' + date + '\_T\.txt'),
 }
@@ -20,37 +21,28 @@ if(regexObject[fileType].test(filename)) {
     console.log('Filename passes');
 } else {
     console.log('Error in filename');
-    if(fileType === '911' && filename.substring(36, 44) !== date) {
+    if((fileType === '911' || fileType === '934') && filename.substring(36, 44) !== date) {
         console.log('incorrect date');
     }
 }
 let count = 0;
+let totalCount = 0;
 
 if(fileType === '911' || fileType === '934') {
-    fs.readFile('./' + filename, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        } else {
-            contents = data.split('\n');
-            let recordCount = 0;
-            contents.forEach((line) => {
-                if(line.includes('</Eligibility>')) {
-                    count++;
-                }
-                if(line.includes('<TotalEligibilityRecords>')) {
+    (async () => {
+      const file = await open(`./${filename}`);
+    
+      for await (const line of file.readLines()) {
+        if(line.includes('</Eligibility>')) {
+            count++;
+        } else if(line.includes('<TotalEligibilityRecords>')) {
                     const start = line.indexOf('>') + 1;
                     const end = line.indexOf('</');
-                    recordCount = line.substring(start, end) * 1;
+                    totalCount = line.substring(start, end) * 1;
                 }
-            })
-            if (recordCount === count) {
-                console.log('record count is correct');
-            } else {
-                console.log('There are ' + count + ' eligibility records in the file but the Trailer count reads ' + recordCount);
-            }
-            
-        }
-    })
+      }
+      console.log('count is ', count);
+      console.log('trailer count is', totalCount);
+      await file.close();
+    })();
 }
-
